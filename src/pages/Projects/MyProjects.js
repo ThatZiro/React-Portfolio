088c43847952912d2ce92244609cc84645f7aca0
Brandon Barnes <Imbrandonbarnes@gmail.com>
2024-01-23 10:51:45 -0800

    Update MyProjects.js

--- src/pages/Projects/MyProjects.js
+++ src/pages/Projects/MyProjects.js
@@ -15,7 +15,7 @@ export default function MyProjects() {
       tech: 'Node.js / MySQL',
       src: '/Tasty-Tableau.png',
       section: 'JS',
-      link: 'https://cryptic-tundra-53497-2c1c36d10121.herokuapp.com/',
+      link: 'https://tasty-tableau-43648013efa7.herokuapp.com/',
       intro: 'Tasty Tableau - Where Every Dish Tells a Story, Yours Included.',
       stack: 'HTML | CSS | JavaScript | Bootstrap | jQuery | Node.js | Nodemon | MySQL | Express.js | Sequelize | Nodemailer',
       desc: 'Welcome to Tasty Tableau, a comprehensive culinary platform that empowers users to explore, discover, and savor a world of recipes. Our app offers a delightful and engaging experience, enabling users to create personalized accounts, search for a wide variety of recipes, access and contribute reviews, and save their favorite recipes for future reference. Whether you\'re a seasoned chef or a home cook looking for culinary inspiration, Tasty Tableau is your go-to destination for a delectable journey through the world of gastronomy. Join us today to embark on your culinary adventure!'
@@ -35,7 +35,7 @@ export default function MyProjects() {
       tech: 'Javascript Mysql',
       src: '/Sub-Project-1.png',
       section: 'JS',
-      link: 'https://warm-tor-21713-78158b52f6f4.herokuapp.com/',
+      link: '',
       intro: 'Learn something today with our intuitive Tech-Blog! Join the community today!',
       stack: 'HTML | CSS | JavaScript | Node.js | Express',
       desc: 'Discover, share, and grow your tech knowledge on our interactive Tech-Blog platform. Create, read, update, and delete your findings on your personal account, contributing to a vibrant community of learners and enthusiasts. Dive into a world of technology insights and stay connected with like-minded individuals. Join us on the journey of continuous learning and exploration!'
@@ -51,14 +51,14 @@ export default function MyProjects() {
       desc: 'Install our basic calculator directly from the web using Webpack and Service Workers. Enjoy a seamless and efficient calculation experience with the power of React. Whether you need simple arithmetic or more complex mathematical operations, our calculator is here to assist you. Try it out and enhance your calculation workflow with modern web technologies!'
     },
     {
-      title: 'Easy Invoice',
+      title: 'Koi Invoicing',
       tech: 'MERN Stack',
       src: '/Sub-Project-3.png',
       section: 'React, Mongo',
-      link: '',
+      link: 'https://koi-invoicing-6238f5ad379e.herokuapp.com/',
       intro: 'Welcome to Easy Invoice, your go-to invoicing platform for freelancers and small businesses!',
       stack: 'MongoDB | Express.js | React | Node.js',
-      desc: 'Easy Invoice is a comprehensive invoicing platform designed to streamline the billing process for freelancers and small businesses. With a powerful MERN stack foundation, we provide a user-friendly interface to create, manage, and send invoices effortlessly. Additionally, our platform allows you to set up subscription plans for recurring billing, making it easy to manage client payments. Stay organized and save time with Easy Invoice as you focus on growing your business. Coming soon!'
+      desc: 'Koi Invoicing is a comprehensive invoicing platform designed to streamline the billing process for freelancers and small businesses. With a powerful MERN stack foundation, we provide a user-friendly interface to create, manage, and send invoices effortlessly. Additionally, our platform allows you to set up subscription plans for recurring billing, making it easy to manage client payments. Stay organized and save time with Easy Invoice as you focus on growing your business. Coming soon!'
     },
     {
       title: 'JavaScript Quiz',
